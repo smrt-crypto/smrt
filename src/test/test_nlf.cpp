2194c9753b258890f1c3a65210700860d70f9059
Karl Erik Fredriksson <43996148+NodelifeKalle@users.noreply.github.com>
2018-10-09 23:20:09 +0200

    Update test_nlf.cpp

--- src/test/test_nlf.cpp
+++ src/test/test_nlf.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#define BOOST_TEST_MODULE Smrt Test Suite
+#define BOOST_TEST_MODULE NodeLife Test Suite
 
 #include "main.h"
 #include "random.h"
