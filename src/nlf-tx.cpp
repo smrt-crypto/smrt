148a3dc72423a0dad98e35bc441ad991242cd883
Karl Erik Fredriksson <43996148+NodelifeKalle@users.noreply.github.com>
2018-10-09 20:23:35 +0200

    Update nlf-tx.cpp

--- src/nlf-tx.cpp
+++ src/nlf-tx.cpp
@@ -46,10 +46,10 @@ static bool AppInitRawTx(int argc, char* argv[])
 
     if (argc < 2 || mapArgs.count("-?") || mapArgs.count("-help")) {
         // First part of help message is specific to this utility
-        std::string strUsage = _("nlf Core smrt-tx utility version") + " " + FormatFullVersion() + "\n\n" +
+        std::string strUsage = _("nlf Core nlf-tx utility version") + " " + FormatFullVersion() + "\n\n" +
                                _("Usage:") + "\n" +
-                               "  nlf-tx [options] <hex-tx> [commands]  " + _("Update hex-encoded smrt transaction") + "\n" +
-                               "  nlf-tx [options] -create [commands]   " + _("Create hex-encoded smrt transaction") + "\n" +
+                               "  nlf-tx [options] <hex-tx> [commands]  " + _("Update hex-encoded nlf transaction") + "\n" +
+                               "  nlf-tx [options] -create [commands]   " + _("Create hex-encoded nlf transaction") + "\n" +
                                "\n";
 
         fprintf(stdout, "%s", strUsage.c_str());
@@ -550,7 +550,7 @@ static int CommandLineRawTx(int argc, char* argv[])
             if (argc < 2)
                 throw runtime_error("too few parameters");
 
-            // param: hex-encoded smrt transaction
+            // param: hex-encoded nlf transaction
             string strHexTx(argv[1]);
             if (strHexTx == "-") // "-" implies standard input
                 strHexTx = readStdin();
