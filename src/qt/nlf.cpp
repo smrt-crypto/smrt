d5b1b37260fef56ca56a96aa08abbbe4341fad11
Karl Erik Fredriksson <43996148+NodelifeKalle@users.noreply.github.com>
2018-10-09 20:57:54 +0200

    Update and rename smrt.cpp to nlf.cpp

--- src/qt/nlf.cpp
+++ src/qt/nlf.cpp
@@ -2,11 +2,12 @@
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The PIVX developers
 // Copyright (c) 2017-2018 The SMRT developers
+// Copyright (c) 2018 The NodeLife developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/smrt-config.h"
+#include "config/nlf-config.h"
 #endif
 
 #include "bitcoingui.h"
@@ -166,7 +167,7 @@ void DebugMessageHandler(QtMsgType type, const QMessageLogContext& context, cons
 }
 #endif
 
-/** Class encapsulating SMRT Core startup and shutdown.
+/** Class encapsulating NodeLife Core startup and shutdown.
  * Allows running startup and shutdown in a different thread from the UI thread.
  */
 class BitcoinCore : public QObject
@@ -195,7 +196,7 @@ private:
     void handleRunawayException(std::exception* e);
 };
 
-/** Main SMRT application object */
+/** Main NodeLife application object */
 class BitcoinApplication : public QApplication
 {
     Q_OBJECT
@@ -253,7 +254,7 @@ private:
     void startThread();
 };
 
-#include "smrt.moc"
+#include "nlf.moc"
 
 BitcoinCore::BitcoinCore() : QObject()
 {
@@ -484,7 +485,7 @@ void BitcoinApplication::initializeResult(int retval)
 
 #ifdef ENABLE_WALLET
         // Now that initialization/startup is done, process any command-line
-        // SMRT: URIs or payment requests:
+        // NodeLife: URIs or payment requests:
         connect(paymentServer, SIGNAL(receivedPaymentRequest(SendCoinsRecipient)),
             window, SLOT(handlePaymentRequest(SendCoinsRecipient)));
         connect(window, SIGNAL(receivedURI(QString)),
@@ -506,7 +507,7 @@ void BitcoinApplication::shutdownResult(int retval)
 
 void BitcoinApplication::handleRunawayException(const QString& message)
 {
-    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occurred. SMRT can no longer continue safely and will quit.") + QString("\n\n") + message);
+    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occurred. NodeLife can no longer continue safely and will quit.") + QString("\n\n") + message);
     ::exit(1);
 }
 
@@ -536,8 +537,8 @@ int main(int argc, char* argv[])
     QTextCodec::setCodecForCStrings(QTextCodec::codecForTr());
 #endif
 
-    Q_INIT_RESOURCE(smrt_locale);
-    Q_INIT_RESOURCE(smrt);
+    Q_INIT_RESOURCE(nlf_locale);
+    Q_INIT_RESOURCE(nlf);
 
     BitcoinApplication app(argc, argv);
 #if QT_VERSION > 0x050100
@@ -587,14 +588,14 @@ int main(int argc, char* argv[])
     /// 6. Determine availability of data directory and parse smrt.conf
     /// - Do not call GetDataDir(true) before this step finishes
     if (!boost::filesystem::is_directory(GetDataDir(false))) {
-        QMessageBox::critical(0, QObject::tr("SMRT Core"),
+        QMessageBox::critical(0, QObject::tr("NodeLife Core"),
             QObject::tr("Error: Specified data directory \"%1\" does not exist.").arg(QString::fromStdString(mapArgs["-datadir"])));
         return 1;
     }
     try {
         ReadConfigFile(mapArgs, mapMultiArgs);
     } catch (std::exception& e) {
-        QMessageBox::critical(0, QObject::tr("SMRT Core"),
+        QMessageBox::critical(0, QObject::tr("NodeLife Core"),
             QObject::tr("Error: Cannot parse configuration file: %1. Only use key=value syntax.").arg(e.what()));
         return false;
     }
@@ -607,7 +608,7 @@ int main(int argc, char* argv[])
 
     // Check for -testnet or -regtest parameter (Params() calls are only valid after this clause)
     if (!SelectParamsFromCommandLine()) {
-        QMessageBox::critical(0, QObject::tr("SMRT Core"), QObject::tr("Error: Invalid combination of -regtest and -testnet."));
+        QMessageBox::critical(0, QObject::tr("NodeLife Core"), QObject::tr("Error: Invalid combination of -regtest and -testnet."));
         return 1;
     }
 #ifdef ENABLE_WALLET
@@ -626,7 +627,7 @@ int main(int argc, char* argv[])
     /// 7a. parse masternode.conf
     string strErr;
     if (!masternodeConfig.read(strErr)) {
-        QMessageBox::critical(0, QObject::tr("SMRT Core"),
+        QMessageBox::critical(0, QObject::tr("NodeLife Core"),
             QObject::tr("Error reading masternode configuration file: %1").arg(strErr.c_str()));
         return false;
     }
@@ -672,7 +673,7 @@ int main(int argc, char* argv[])
         app.createWindow(networkStyle.data());
         app.requestInitialize();
 #if defined(Q_OS_WIN) && QT_VERSION >= 0x050000
-        WinShutdownMonitor::registerShutdownBlockReason(QObject::tr("SMRT Core didn't yet exit safely..."), (HWND)app.getMainWinId());
+        WinShutdownMonitor::registerShutdownBlockReason(QObject::tr("NodeLife Core didn't yet exit safely..."), (HWND)app.getMainWinId());
 #endif
         app.exec();
         app.requestShutdown();
